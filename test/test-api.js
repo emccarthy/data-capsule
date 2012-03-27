7c56c902fff37403409d1c9931f0cdb8cb9d4305
Richard Rodger <richard@ricebridge.com>
2012-03-27 12:00:10 +0100

    sync

--- test/test-api.js
+++ test/test-api.js
@@ -6,25 +6,48 @@ var host = process.argv[2]
 var port = process.argv[3]
 
 var base = 'http://'+host+':'+port+'/capsule'
-var rest_base = base+'/rest'
+console.log(base)
 
-console.log(rest_base)
+var rest_base = base+'/rest'
+var sync_base = base+'/sync'
+
+
 
 var printres = function(err,res,body) {
   body = 'string'==typeof(body)?JSON.parse(body):body
   console.log(err+' '+res.statusCode+' '+JSON.stringify(body))
 }
 
+
+function test() {
+
+  console.log('\n\nACTIONS:')
+
 ;request.post({uri:rest_base+'/ac1/coll1/foo=bar',json:{b:20}}, function(err,res,body) {
   printres(err,res,body)
   var item = body
 
-;request.post({uri:rest_base+'/ac1/coll1/foo=bar',json:{a:10,index$:0}}, function(err,res,body) {
+;request.post({uri:rest_base+'/ac1/coll1/foo=bar',json:{a:10}}, function(err,res,body) {
   printres(err,res,body)
   var item = body
 
 ;request.get({uri:rest_base+'/ac1/coll1/foo=bar/'+item.id}, function(err,res,body) {
   printres(err,res,body)
+
+
+  console.log('\n\nHISTORY:')
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/version'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/history/0'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/0'}, function(err,res,body) {
+  printres(err,res,body)
+
+
+  console.log('\n\nACTIONS:')
 
 ;request.get({uri:rest_base+'/ac1/coll1/foo=bar'}, function(err,res,body) {
   printres(err,res,body)
@@ -37,10 +60,56 @@ var printres = function(err,res,body) {
   printres(err,res,body)
 
 
+  console.log('\n\nHISTORY:')
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/version'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/history/0'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/0'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/1'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/2'}, function(err,res,body) {
+  printres(err,res,body)
+
+
+  console.log('\n\nACTIONS:')
+
 ;request.del({uri:rest_base+'/ac1/coll1/foo=bar/'+item.id}, function(err,res,body) {
   printres(err,res,body)
 
 ;request.get({uri:rest_base+'/ac1/coll1/foo=bar'}, function(err,res,body) {
   printres(err,res,body)
 
-}) }) }) }) }) }) }) })
+
+  console.log('\n\nHISTORY:')
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/version'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/history/0'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/0'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/1'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/2'}, function(err,res,body) {
+  printres(err,res,body)
+
+;request.get({uri:sync_base+'/ac1/coll1/foo=bar/updates/3'}, function(err,res,body) {
+  printres(err,res,body)
+
+
+}) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) }) 
+
+}
+
+test()
